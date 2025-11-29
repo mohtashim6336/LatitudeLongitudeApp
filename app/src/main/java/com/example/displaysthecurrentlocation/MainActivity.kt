595aa67afb4f5d7049560794639d120f5ce03656
M MOHTASHIM SAJID <110852571+mohtashim6336@users.noreply.github.com>
2025-11-29 22:05:26 +0500

    Update MainActivity.kt

--- app/src/main/java/com/example/displaysthecurrentlocation/MainActivity.kt
+++ app/src/main/java/com/example/displaysthecurrentlocation/MainActivity.kt
@@ -15,7 +15,7 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var fusedLocationClient: FusedlocationProviderClient
+    private lateinit var fusedLocationClient: FusedlocationproviderClient
     private lateinit var locationTextView: TextView
     private lateinit var cityTextView: TextView
     private lateinit var refreshButton: Button
